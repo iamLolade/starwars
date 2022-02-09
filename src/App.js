eafda0c514919fb33943f939b7fa8a728edd69a0
iamLolade <adeniyiotemade@gmail.com>
2022-02-09 23:51:23 +0100

    Created the required components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import People from "./components/People";
+import Planets from "./components/Planets";
+
 
 function App() {
+  const [page, setPage] = useState("planets")
   return (
     <div className="App">
-      
+      <h1>Star Wars</h1>
+      <Navbar setPage={setPage} />
+      <div className="content">
+        {page === "planets" ? <Planets /> : <People />}
+      </div>
     </div>
   );
 }
