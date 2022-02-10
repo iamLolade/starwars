545c97dbf67966e577693c4d8106654d56579b57
iamLolade <adeniyiotemade@gmail.com>
2022-02-10 13:51:06 +0100

    Updated People and Planets component | Implemented pagination

--- src/components/People.js
+++ src/components/People.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import Person from "./card/Person";
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { usePaginatedQuery } from 'react-query';
 
-const queryClient = new QueryClient();
+//const queryClient = new QueryClient();
 
-const fetchPeople = async () => {
-    const result = await fetch('https://swapi.dev/api/people')
+const fetchPeople = async (key, page) => {
+    const result = await fetch(`https://swapi.dev/api/people/?page=${page}`)
     return result.json()
 }
 
 const People = () => {
-    const { data, status } = useQuery('people', fetchPeople)
-    //console.log("data", data, "status", status)
+    const [page, setPage] = useState(1)
+    const { 
+        resolvedData, 
+        latestData, 
+        status 
+    } = usePaginatedQuery(['people', page], fetchPeople)
     return (
         <div>
             <h2>People</h2>
@@ -24,18 +28,29 @@ const People = () => {
             )}
 
             {status === "success" && (
-                <div>
-                    { data.results.map(person => <Person key={person.name} person={person}/>)}
-                </div>
+                <>
+                    <button
+                        onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+                        disabled={page === 1}
+                    >Previous</button>
+                    <span>{ page }</span>
+                    <button
+                        onClick={() => setPage(prev => (!latestData || !latestData.next ? prev : prev + 1))}
+                        disabled={!latestData || !latestData.next}
+                    >Next</button>
+                    <div>
+                        { resolvedData.results.map(person => <Person key={person.name} person={person}/>)}
+                    </div>
+                </>
             )}
         </div>
     );
 }
 
-
-export default function Wraped() {
-    return (<QueryClientProvider client={queryClient}>
-        <People />
-    </QueryClientProvider>
-    );
-}
+export default People
+// export default function Wraped() {
+//     return (<QueryClientProvider client={queryClient}>
+//         <People />
+//     </QueryClientProvider>
+//     );
+// }
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import Planet from "./card/Planet";
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider, usePaginatedQuery } from 'react-query';
 
-const queryClient = new QueryClient();
+//const queryClient = new QueryClient();
 
-const fetchPlanets = async () => {
-    const result = await fetch('https://swapi.dev/api/planets')
+const fetchPlanets = async (key, page) => {
+    const result = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
     return result.json()
 }
 
 const Planets = () => {
-    const { data, status } = useQuery('planets', fetchPlanets)
-    //console.log("data", data, "status", status)
+    const [page, setPage] = useState(1);
+    const { 
+        resolvedData,
+        latestData,
+        status 
+    } = usePaginatedQuery(['planets', page], fetchPlanets)
     return (
         <div>
             <h2>Planets</h2>
@@ -24,18 +28,29 @@ const Planets = () => {
             )}
 
             {status === "success" && (
-                <div>
-                    { data.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
-                </div>
+                <>
+                    <button
+                        onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+                        disabled={page === 1}
+                    >Previous</button>
+                    <span>{ page }</span>
+                    <button
+                        onClick={() => setPage(prev => (!latestData || !latestData.next ? prev : prev + 1))}
+                        disabled={!latestData || !latestData.next}
+                    >Next</button>
+                    <div>
+                        { resolvedData.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
+                    </div>
+                </>
             )}
         </div>
     );
 }
 
-//export default Planets
-export default function Wraped() {
-    return (<QueryClientProvider client={queryClient}>
-        <Planets />
-    </QueryClientProvider>
-    );
-}
+export default Planets
+// export default function Wraped() {
+//     return (<QueryClientProvider client={queryClient}>
+//         <Planets />
+//     </QueryClientProvider>
+//     );
+// }
