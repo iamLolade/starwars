336676659b0a8da1ed1eea7035dec4c41285ccc9
iamLolade <adeniyiotemade@gmail.com>
2022-02-10 10:33:58 +0100

    Updated Planets component and created Planet card component

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,21 +1,41 @@
-import React from "react"
-import { useQuery } from "react-query";
+import React, { useState }from "react";
+import Planet from "./custom/Planet";
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const fetchPlanets = async () => {
-    const result = await fetch("http://swapi.dev/api/planets");
-    return result.json();
+    const result = await fetch('https://swapi.dev/api/planets')
+    return result.json()
 }
 
 const Planets = () => {
-    const { data, status } = useQuery("planets", fetchPlanets)
-    console.log(data);
-
-    return ( 
+    const { data, status } = useQuery('planets', fetchPlanets)
+    //console.log("data", data, "status", status)
+    return (
         <div>
             <h2>Planets</h2>
-            <p>{ status }</p>
+            {status === "loading" && (
+                <div>Loading data...</div>
+            )}
+
+            {status === "error" && (
+                <div>Error fetching data</div>
+            )}
+
+            {status === "success" && (
+                <div>
+                    { data.results.map(planet => <Planet key={planet.name} planet={planet}/>)}
+                </div>
+            )}
         </div>
-     );
+    );
 }
- 
-export default Planets;
+
+//export default Planets
+export default function Wraped() {
+    return (<QueryClientProvider client={queryClient}>
+        <Planets />
+    </QueryClientProvider>
+    );
+}
